c7476aea8a6e71756db3de9498d07a5c7c793570
Hanif8193 <hanifdostmuhammad8193@gmail.com>
2024-11-12 19:02:27 +0500

    my-web

--- src/app/components/Poster.tsx
+++ src/app/components/Poster.tsx
@@ -4,7 +4,7 @@ import p1 from '../../../public/p1.png'
 
 function Poster() {
   return (
-    <div style={{ backgroundImage: "url('/bg.png')" }}>
+    <div style={{ backgroundImage: "url('/bg.png')" }} className='f'>
       <Image src={p1} alt='IMG'  width={1000}height={1000}/>
     </div>
   )
