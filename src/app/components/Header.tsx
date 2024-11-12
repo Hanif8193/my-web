7125114727bed009a49be866df5e688810edd6dd
Hanif8193 <hanifdostmuhammad8193@gmail.com>
2024-11-12 13:12:11 +0500

    first commit

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import {Bungee} from 'next/font/google'
+
+const bungee = Bungee({
+  weight:"400",
+  subsets:['latin'],
+  display: "swap"
+})
+
 
 const Header = () => {
   return (
-    <div style={{backgroundImage: "url('/Bg.png')"}} className='border-t py-10 items-center px-10'>
-      <h1 className='text-white sm:text-1xl md:text-2xl lg:  text-6xl font-bold px-24 '>SOCIAL MEDIA</h1>
-      <h1 className='text-yellow-500  sm:text-1xl md:text-2xl text-6xl font-bold px-32'>MARKETING</h1>
+    <div className={bungee.className}>
+    <div style={{backgroundImage: "url('/Bg.png')"}} className='border-t py-10 items-center px-14 tracking-widest'>
+      <h1 className=' text-white sm:text-1xl md:text-2xl lg:  text-5xl font-bold px-24 '>SOCIAL MEDIA</h1>
+      <h1 className=' text-yellow-500  sm:text-1xl md:text-2xl text-5xl font-bold px-32'>MARKETING</h1>
     </div>
+     </div>
   )
 }
 
